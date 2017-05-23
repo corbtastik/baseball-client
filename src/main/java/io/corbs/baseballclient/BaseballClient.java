2ae314121ef7c6031113f0484f34b9209cd441a5
Corbett Martin <corbtastik@gmail.com>
2017-05-23 11:02:49 -0500

    baseball client

--- src/main/java/io/corbs/baseballclient/BaseballClient.java
+++ src/main/java/io/corbs/baseballclient/BaseballClient.java
@@ -1,16 +1,16 @@
 package io.corbs.baseballclient;
 
+import cworks.json.Json;
+import cworks.json.JsonArray;
+import cworks.json.JsonObject;
 import net.cworks.http.Http;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.util.StringUtils;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
-import java.util.*;
+import java.util.Date;
+import java.util.Random;
 
 @RestController
 public class BaseballClient {
@@ -18,55 +18,53 @@ public class BaseballClient {
     @Value("${api.root}")
     private String apiRoot;
 
-    private static Set<String> players = new HashSet<>();
-
     private Random randomizer = new Random();
 
     /**
      * Call the player REST API
      */
-    @Scheduled(fixedDelay=5000)
     public void callPlayerApi() {
 
         try {
-            if(players.size() > 0) {
-                String response = Http.get(apiRoot + "/players/player/" + nextPlayerId()).asString();
-                System.out.println(new Date() + ": " + response);
+            String playerId = nextPlayer();
+            if(playerId == null) {
+                return;
             }
+            String response = Http.get(apiRoot + "/players/player/" + nextPlayer()).asString();
+            System.out.println(new Date() + ": " + response);
         } catch (IOException ex) {
             ex.printStackTrace();
         }
     }
 
     /**
-     * Add a player into this client's memory
-     * @param playerId
+     * Call the player REST API
      */
-    @RequestMapping(method = RequestMethod.POST, path="/players/{playerId}")
-    void addPlayer(@PathVariable String playerId) {
-        if(!StringUtils.isEmpty(playerId)) {
-            players.add(playerId);
+    @Scheduled(fixedDelay=5000)
+    public void callRandomPlayerAPI() {
+
+        try {
+            String response = Http.get(apiRoot + "/players/players/random").asString();
+            System.out.println(new Date() + ": " + response);
+        } catch (IOException ex) {
+            ex.printStackTrace();
         }
     }
 
-    /**
-     * Remove a player from this client's memory
-     * @param playerId
-     */
-    @RequestMapping(method = RequestMethod.DELETE, path="/players/{playerId}")
-    void removePlayer(@PathVariable String playerId) {
-        if(!StringUtils.isEmpty(playerId)) {
-            players.remove(playerId);
-        }
-    }
+    private String nextPlayer() throws IOException {
 
-    /**
-     * Return a random playerId
-     * @return
-     */
-    private String nextPlayerId() {
-        int i = randomizer.nextInt(players.size());
-        return String.valueOf(players.toArray()[i]);
+        String response = Http.get(apiRoot + "/players/players").asString();
+        JsonObject responseObject = Json.asObject(response);
+
+        JsonArray players = responseObject.getArray("data");
+        if(players == null) {
+            return null;
+        }
+
+        JsonObject player = responseObject.getArray("data").get(
+            randomizer.nextInt(responseObject.getArray("data").size()));
+
+        return player.getString("id");
     }
 
 }
